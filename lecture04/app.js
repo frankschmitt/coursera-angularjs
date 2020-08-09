cc0832961114f7665c460ac3fc40a9633c865262
Frank Schmitt <frankschmitt@gmx.de>
2020-08-09 15:49:48 +0200

    Lecture 6: reacting to key events

--- lecture04/app.js
+++ lecture04/app.js
@@ -1,11 +1,23 @@
 (function() {
   'use strict';
 
-  angular.module("myFirstApp", [])
-  .controller("MyFirstController", function ($scope) {
-    $scope.name = "Frank";
-    $scope.sayHello = function() {
-      return "Hello Coursera!";
+  angular.module("NameCalculator", [])
+  .controller("NameCalculatorController", function ($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function() {
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
     };
   });
+
+
+  function calculateNumericForString(s) {
+    var totalStringValue = 0;
+    for (var i = 0; i < s.length; i++) {
+      totalStringValue += s.charCodeAt(i);
+    }
+    return totalStringValue;
+  };
 })();
